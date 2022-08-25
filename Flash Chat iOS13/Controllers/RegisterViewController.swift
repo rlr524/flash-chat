019c30ac9925aa25488bca2da35a8d2f574bf373
Rob Ranf <rob@emiyaconsulting.com>
2022-08-25 11:29:05 -0700

    Adding CLTypingLabel pod

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -12,8 +12,7 @@ class RegisterViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
-    
+
     @IBAction func registerPressed(_ sender: UIButton) {
     }
-    
 }
