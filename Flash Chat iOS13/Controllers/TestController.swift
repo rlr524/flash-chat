2265d4c7f1846efea76adec61344df4865cdc72e
Rob Ranf <rob@emiyaconsulting.com>
2023-01-29 18:04:39 -0800

    Updated headings

--- Flash Chat iOS13/Controllers/TestController.swift
+++ Flash Chat iOS13/Controllers/TestController.swift
@@ -2,8 +2,5 @@
 //  TestController.swift
 //  Flash Chat iOS13
 //
-//  Created by Rob Ranf on 8/30/22.
-//  Copyright © 2022 Angela Yu. All rights reserved.
-//
 
 import Foundation
--- Flash Chat iOS13/Models/Message.swift
+++ Flash Chat iOS13/Models/Message.swift
@@ -2,9 +2,6 @@
 //  Message.swift
 //  Flash Chat iOS13
 //
-//  Created by Rob Ranf on 12/11/22.
-//  Copyright © 2022 Angela Yu. All rights reserved.
-//
 
 import Foundation
 
